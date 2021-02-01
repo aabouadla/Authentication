d5156c9734d770157831df3f8df36c4f2d9caf96
aabouadla <aabouadla@gmail.com>
2021-02-01 11:19:19 +0100

    Add project files.

---
+++ Basics/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Basics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var grandmaClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "ABED"),
+                new Claim(ClaimTypes.Email, "aabouadla@gmail.com"),
+                new Claim("Grandma.Says", "Very nice boi."),
+            };
+
+            var licenseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Abdulrahman Abou Adla"),
+                new Claim(ClaimTypes.Email, "aabouadla@gmail.com"),
+                new Claim("DrivingLicense", "A+"),
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+            var LicenseIdentity = new ClaimsIdentity(licenseClaims, "Govermet");
+
+            var userPrinciple = new ClaimsPrincipal(new[] { grandmaIdentity, LicenseIdentity});
+
+            HttpContext.SignInAsync(userPrinciple);
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Basics/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Grandmas.Cookie
+            // Specifying a Cookie Handler to produce an implementation of an Authentication Handler
+            // it will be injected in our service and will be used in app.UseAuthorization 
+            // to check if the User Authenticated or not
+            services.AddAuthentication("CookieAuth")
+                .AddCookie("CookieAuth", config =>
+                {
+                    config.Cookie.Name = "Grandmas.Cookie";
+                    config.LoginPath = "/Home/Authenticate";
+                });
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // who are you
+            app.UseAuthentication();
+
+            // are you Authoreized (allowed)
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
