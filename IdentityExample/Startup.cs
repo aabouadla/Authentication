7fe14b3f81902be43d0a2022ec1b77d6f31d7aad
aabouadla <aabouadla@gmail.com>
2021-02-01 15:02:46 +0100

    Basics Authentication from  Ep. 1 AspNet Core Identity Example from  Ep. 2

---
+++ IdentityExample/Startup.cs
@@ -0,0 +1,80 @@
+using IdentityExample.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityExample
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(config =>
+            {
+                config.UseInMemoryDatabase("Memo");
+            });
+
+            // AddIdentity registers the services
+            services.AddIdentity<IdentityUser, IdentityRole>(config =>
+            {
+                config.Password.RequiredLength = 4;
+                config.Password.RequireDigit = false;
+                config.Password.RequireUppercase = false;
+                config.Password.RequireNonAlphanumeric = false;
+            })
+                .AddEntityFrameworkStores<AppDbContext>() // to let the Identity service know to which Db that sghoul communicate
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(config =>
+            {
+                config.Cookie.Name = "Identity.Cookie";
+                config.LoginPath = "/Home/Login";
+            });
+            // Grandmas.Cookie
+            // Specifying a Cookie Handler to produce an implementation of an Authentication Handler
+            // it will be injected in our service and will be used in app.UseAuthorization 
+            // to check if the User Authenticated or not
+            //services.AddAuthentication("CookieAuth")
+            //    .AddCookie("CookieAuth", config =>
+            //    {
+            //        config.Cookie.Name = "Grandmas.Cookie";
+            //        config.LoginPath = "/Home/Authenticate";
+            //    });
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // who are you
+            app.UseAuthentication();
+
+            // are you Authoreized (allowed)
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
